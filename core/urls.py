9b3fe69e353f9a9ecd107c4f5c40118c2c700144
Alexandre Marinho <lyralemos@gmail.com>
2014-09-04 09:42:17 -0300

    Adicionado feed rss e suporte ao fabric

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 from views import EntryListView, EntryDetailView
+from feed import LatestEntriesFeed
 
 urlpatterns = patterns('',
+    url(r'^rss/$', LatestEntriesFeed(),name= 'rss'),
     url(r'^(?P<slug>[-_\w]+)/$', EntryDetailView.as_view(), name='entry-detail'),
     url(r'^$',EntryListView.as_view(),name='index'),
 )
