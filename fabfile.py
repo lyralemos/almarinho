3fe36ca849d2f846d9e8d2591ca01ee3f33f30cd
Alexandre Marinho <lyralemos@gmail.com>
2014-09-04 10:25:46 -0300

    Adicionado o framework para sites

--- fabfile.py
+++ fabfile.py
@@ -2,11 +2,12 @@ from fabric.api import local, settings, abort, run, cd
 from fabric.contrib.console import confirm
 
 def prepare_deploy():
-    local("git add -p && git commit")
+    local("git add --all && git commit")
     local("git push")
 
 def deploy():
+    prepare_deploy()
     code_dir = '/home/almarinho/almarinho'
     with cd(code_dir):
-        run("git pull")
-        run("touch app.wsgi")
+        run('su - almarinho -c "git pull"')
+        run("supervisorctl restart almarinho")
